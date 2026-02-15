9c7285c95eb083815bc41df70253719276dcf814
harrisonkim987 <harrisonkim987@gmail.com>
2026-02-15 01:58:13 +0000

    finished lesson 1

---
+++ lesson1/classwork/printing.py
@@ -0,0 +1,6 @@
+print("hello world!")
+print("im harrison")
+print('i like pizza')
+
+#print hi
+# error quotation marks mismatched
---
+++ lesson1/classwork/variables.py
@@ -0,0 +1,11 @@
+x = 5 # integer: whole number
+print(x)
+
+favorite_food = "pizza"
+print(favorite_food)
+
+print (x, favorite_food)
+print ("my favorite food is", favorite_food)
+
+y = 6.767 # float: decimal point number
+print(y, "is a float", 67)
