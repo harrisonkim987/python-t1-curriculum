28f8e598819c7840faf3db68b3affed2ab0af91d
harrisonkim987 <harrisonkim987@gmail.com>
2026-02-22 04:59:49 +0000

    finished homework lesson 2

--- lesson2/homework/homework.py
+++ lesson2/homework/homework.py
@@ -1,15 +1,17 @@
 # Homework Problem 1
 # Ask the user for two numbers.
 # Print their quotient and remainder on separate lines.
-
-
-
+first_number = int(input("first number"))
+second_number = int(input("second number"))
+quotient = (int(first_number / second_number))
+print(quotient)
+print("with remainder", first_number - (quotient * second_number))# this was not fun
 # Homework Problem 2
 # Ask the user for their favorite animal and favorite color.
 # Print a sentence combining them like: "A blue tiger would be awesome!"
-
-
-
+favanimal = input("favorite animal")
+favcolor = input("favorite color")
+print(favcolor, favanimal, "s are cool")
 # Homework Problem 3
 # Use a for loop to print all the even numbers from 0 to 10 (including 10).
 
@@ -18,9 +20,8 @@
 # Homework Problem 4
 # Ask the user how many push-ups they can do.
 # Multiply it by 7 and print how many they could do in a week.
-
-
-
+pushups = input(pushup count:)
+print(pushups * 7)
 # Homework Problem 5
 # Use a for loop to print the square of each number from 1 to 6.
 # (Example: 1*1=1, 2*2=4, etc.)
